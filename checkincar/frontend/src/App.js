8d2c78f340a9a5f09e522054a780b21570297130
Dmitriy Kardasevskiy <kardashevskiy@anc.space>
2023-04-10 19:03:46 +0300

    first commit

---
+++ checkincar/frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { FileUploader } from "./file-uploader";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>File Uploader</h1>
+      <FileUploader />
+    </div>
+  );
+}
